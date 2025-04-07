449dfc6d74741bcae0c7a2cc60e36eb9caaea047
Gabriel Pascoal <gabriel.pascoal@green4t.com>
2025-04-07 12:12:06 -0300

    feat: Update entrypoint.sh for GLPI installation and permissions adjustments

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# green4T 2024
-# Gabriel Pascoal - gabriel.pascoal@green4t.com 
+# green4T 2025
+# Gabriel Pascoal - gabriel.pascoal@green4t.com
 
 set -x
 
@@ -9,6 +9,13 @@ log() {
 }
 
 glpi_install() {
+  log "INFO: Adjusting permissions"
+  chown -R apache:apache ${GLPI_HOME} ${GLPI_VAR_DIR} /var/log/glpi
+
+  log "INFO: Adjusting cache permissions"
+  mkdir -p ${GLPI_VAR_DIR}/_cache
+  chmod -R 775 ${GLPI_VAR_DIR}/_cache
+
   log "INFO: Installing DB GLPI"
   php ${GLPI_HOME}/bin/console glpi:database:install \
     --db-host ${DB_HOST} \
@@ -20,11 +27,14 @@ glpi_install() {
     --no-interaction \
     --reconfigure \
     --force -vvv
+
+  log "INFO: Adjusting permissions after install"
+  chown -R apache:apache ${GLPI_HOME} ${GLPI_VAR_DIR} /var/log/glpi ${GLPI_VAR_DIR}/_cache
 }
 
-if [ ! -s /etc/glpi/config_db.php ]
-then
+if [ ! -s /etc/glpi/config_db.php ]; then
   log "INFO: Installing GLPI"
   glpi_install
 fi
+
 /usr/local/bin/start.sh
